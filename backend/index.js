3e5919eeeee0996cbb36c9e958c825e05f4ac336
Eliezer Perl <eliezerrules@gmail.com>
2024-02-26 10:50:03 +0200

    dploy

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import serverless from 'serverless-http';
 import cors from 'cors';
 import mongoose from 'mongoose';
 import seedRouter from './routes/seedRouter.js';
@@ -8,13 +9,13 @@ import orderRouter from './routes/orderRouter.js';
 import dotenv from 'dotenv';
 
 dotenv.config();
-const PORT = process.env.PORT || 8080;
+// const PORT = process.env.PORT || 8080;
 
 const app = express();
 app.use(cors()); // does nothing at the moment
 app.use(express.json()); // parses JSONs
-app.use(express.urlencoded({ extended: false })); //this is common practice for urlencoded
-// these three lines are boilerplate
+app.use(express.urlencoded({ extended: false })); 
+
 
 //middleware
 //routes:
@@ -26,13 +27,15 @@ app.use((err, req, res, next) => {
   res.status(500).send({ message: err.message });
 });
 
-mongoose
-  .connect(process.env.MONGO)
-  .then(() => {
-    app.listen(PORT, function () {
-      console.log('listening on ' + PORT);
-    });
-  })
-  .catch((err) => {
-    console.log(err.message);
-  });
+export const handler = serverless(app);
+
+// mongoose
+//   .connect(process.env.MONGO)
+//   .then(() => {
+//     app.listen(PORT, function () {
+//       console.log('listening on ' + PORT);
+//     });
+//   })
+//   .catch((err) => {
+//     console.log(err.message);
+//   });
